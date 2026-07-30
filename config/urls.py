4760311bd54901e5d28cff6759dae572481eb1af
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-07-31 06:23:03 +0700

    Final commit

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 import catalog
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog')),
+    path("", include("catalog.urls", namespace="catalog")),
 ]
