69730ef2ca62664e7d11addcc21c72301c606f8c
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-09-12 10:14:48 +0700

    Task 1.2. Complete

--- catalog/management/commands/create_groups.py
+++ catalog/management/commands/create_groups.py
@@ -1,20 +1,33 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
+from catalog.models import Product
 
 
 class Command(BaseCommand):
-  help = 'Создание базовых групп пользователей'
+  help = (
+      "Создание группы 'Модератор продуктов' и назначение ей прав доступа"
+  )
 
   def handle(self, *args, **options):
-    groups = ['Модераторы', 'Менеджеры']
+    # 1. Создаем или получаем группу
+    moderator_group, created = Group.objects.get_or_create(name="Модератор продуктов" )
+    if created:
+      self.stdout.write(self.style.SUCCESS('Группа "Модератор продуктов" успешно создана.'))
+    else:
+      self.stdout.write(self.style.WARNING('Группа "Модератор продуктов" уже существует. Обновляем права...'))
 
-    for group_name in groups:
-      group, created = Group.objects.get_or_create(name=group_name)
-      if created:
-        self.stdout.write(
-            self.style.SUCCESS(f'Группа "{group_name}" успешно создана.')
-        )
-      else:
-        self.stdout.write(
-            self.style.WARNING(f'Группа "{group_name}" уже существует.')
-        )
+    # 2. Получаем ContentType для модели Product
+    product_content_type = ContentType.objects.get_for_model(Product)
+
+    # 3. Собираем кодовые имена прав, которые нужно назначить: кастомное право на отмену публикации,
+    # - встроенное право на удаление продукта (Django создает его автоматически: delete_<model_name>)
+    permission_codenames = ["can_unpublish_product", f"delete_{Product._meta.model_name}",]
+
+    # 4. Находим эти разрешения в базе данных
+    permissions = Permission.objects.filter(content_type=product_content_type, codename__in=permission_codenames)
+
+    # 5. Назначаем найденные права группе
+    moderator_group.permissions.set(permissions)
+
+    self.stdout.write(self.style.SUCCESS("Права успешно назначены группе 'Модератор продуктов'."))
