eb87ce808ca44cc0b12451315842d72b87b842cb
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-09-12 09:53:48 +0700

    Pre Task Complete

---
+++ catalog/management/commands/create_groups.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import Group
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+  help = 'Создание базовых групп пользователей'
+
+  def handle(self, *args, **options):
+    groups = ['Модераторы', 'Менеджеры']
+
+    for group_name in groups:
+      group, created = Group.objects.get_or_create(name=group_name)
+      if created:
+        self.stdout.write(
+            self.style.SUCCESS(f'Группа "{group_name}" успешно создана.')
+        )
+      else:
+        self.stdout.write(
+            self.style.WARNING(f'Группа "{group_name}" уже существует.')
+        )
--- catalog/models.py
+++ catalog/models.py
@@ -15,6 +15,11 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'draft', 'Черновик'
+        PUBLISHED = 'published', 'Опубликовано'
+
     name = models.CharField(max_length=150, verbose_name="Наименование")
     description = models.TextField(verbose_name="Описание", blank=True, null=True)
     image = models.ImageField(upload_to="products/photo", verbose_name="Изображение", blank=True, null=True)
@@ -22,6 +27,10 @@ class Product(models.Model):
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.DRAFT, verbose_name='Статус публикации',)
+
+
+
 
     def __str__(self):
         return self.name
